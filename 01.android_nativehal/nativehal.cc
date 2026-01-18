8418ba8bd2103384be160b91f92d983dd902fee1
kaspersigi <kaspersigi@outlook.com>
2026-01-18 22:25:36 +0800

    update shell cmd arg

--- 01.android_nativehal/nativehal.cc
+++ 01.android_nativehal/nativehal.cc
@@ -2,7 +2,14 @@
 #include <chrono>
 #include <thread>
 
-// export LD_LIBRARY_PATH=/vendor/lib64:/system/lib64:/apex/com.android.vndk.v30/lib64:/apex/com.android.vndk.v31/lib64:/apex/com.android.vndk.v32/lib64:/apex/com.android.vndk.v33/lib64:$LD_LIBRARY_PATH
+// mv /system/bin/cameraserver /system/bin/cameraserver.bak
+// mv /vendor/bin/hw/vendor.qti.camera.provider@2.7-service_64 /vendor/bin/hw/vendor.qti.camera.provider@2.7-service_64.bak
+// stop cameraserver
+// pkill -f camera*
+
+// adb push /mnt/develop/android-ndk-r27d/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/aarch64-linux-android/libc++_shared.so /data/local/tmp/
+// export LD_LIBRARY_PATH=/system/lib64:.:/vendor/lib64:/apex/com.android.vndk.v30/lib64:/apex/com.android.vndk.v31/lib64:/apex/com.android.vndk.v32/lib64:/apex/com.android.vndk.v33/lib64
+// ./nativehal
 
 auto main(int argc, char* argv[]) -> int
 {
